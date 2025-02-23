1b068b241b51783c9f6b89d156e52b845bab1f99
Henry <henrygl450@yahoo.com>
2025-02-23 11:21:10 -0500

    0.4

---
+++ react-cc-3/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "7192ef2172c2ee0d530a454d49a28ca7";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
